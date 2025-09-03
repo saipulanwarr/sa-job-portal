0ca41e2f28ca16e410deda74b67ae5fdfb006aed
saipulanwarr <saipulanwar180994@gmail.com>
2025-09-03 20:48:43 +0700

    working with admin logout feature

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -43,12 +43,12 @@
                         </x-dropdown-link>
 
                         <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
+                        <form method="POST"
+                            action="{{ Auth()->guard('admin')->check() ? route('admin.logout') : route('logout') }}">
                             @csrf
 
                             <x-dropdown-link :href="route('logout')"
-                                onclick="event.preventDefault();
-                                                this.closest('form').submit();">
+                                onclick="event.preventDefault(); this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
